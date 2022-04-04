953d2aa5f0cd663f840262cfecc03324fc6d3fc6
KennetAguirreReyes <85863289+KennetAguirreReyes@users.noreply.github.com>
2022-04-03 20:27:59 -0700

    domain

---
+++ Domain/Entities/Activo.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Activo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public double Valor { get; set; }
+        public int VidaUtil { get; set; }
+        public double ValorResidual { get; set; }
+        public string Codigo { get; set; }
+        public int IdEmpleado { get; set; }
+        public string EstadoActivo { get; set; }
+
+    }
+}
